483c8a2a0eb2e1c400dd6c6b7de11a57f2f7b5ad
Andrii Votiakov <andriivotiakov@Andriis-MacBook-Pro.local>
2017-09-08 14:27:56 +0200

    Hyperloop patch

--- lib/annotate/active_record_patch.rb
+++ lib/annotate/active_record_patch.rb
@@ -7,3 +7,31 @@ module ::ActiveRecord
     end
   end
 end
+
+class Object
+	class << self
+		def self.method_missing(_name, *_args)
+		  # ignore this, so unknown/unloaded macros won't cause parsing to fail
+		end
+
+		def method_missing(_name, *_args)
+		  # ignore this, so unknown/unloaded macros won't cause parsing to fail
+		end
+	end
+
+	def self.method_missing(_name, *_args)
+	  # ignore this, so unknown/unloaded macros won't cause parsing to fail
+	end
+
+	def method_missing(_name, *_args)
+	  # ignore this, so unknown/unloaded macros won't cause parsing to fail
+	end
+end
+
+
+module ::Hyperloop
+	class Router
+		class Component
+		end
+	end
+end
